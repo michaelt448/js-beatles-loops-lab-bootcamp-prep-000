084a59adf4b73d2bdbed641df97477a118da51df
michael <michaeltilman77@gmail.com>
2018-06-08 18:49:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments){
   var array = []
   for(var i = 1; i < musicians.length; i++){
     array.push(musicians[i] + " plays " + instruments[i])
-    console.log(array)
+  
   return array;
 }
 }
