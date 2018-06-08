1e607882dbc74ff8073fadcbd7a3a2f4f8ba448f
michael <michaeltilman77@gmail.com>
2018-06-08 18:31:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  for(var i = 0; i < musicians.length()-1; i++){
+    array.push(musicians[i] + " plays " + instruments[i])
+    console.log(array)
+  return array;
+}
+}
+function johnLennonFacts(facts){
+  
+}
+function iLoveTheBeatles(number){
+  
+}
