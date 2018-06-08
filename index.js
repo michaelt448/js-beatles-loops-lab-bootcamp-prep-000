21a9fae3408faf41111fc72d8fb373b91c79f769
michael <michaeltilman77@gmail.com>
2018-06-08 18:42:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var array = []
-  for(var i = 0; i < musicians.length-1; i++){
+  for(var i = 0; i < musicians.length; i++){
     array.push(musicians[i] + " plays " + instruments[i])
     console.log(array)
   return array;
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts){
   var excitingFacts = []
   while (facts.length >0){
-    excitingFacts.push(fact[0] + "!!!")
+    excitingFacts.push(facts[0] + "!!!")
     facts.unshift()
   }
   return excitingFacts
@@ -20,6 +20,6 @@ function iLoveTheBeatles(number){
   do{
     statements.push("I love the Beatles!")
     iteration++
-  } while(i < 15)
+  } while(iteration < 15)
   return statements
 }
