64c559fdbd3526663220292d2a082525e142af23
michael <michaeltilman77@gmail.com>
2018-06-08 18:49:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function theBeatlesPlay(musicians, instruments){
   var array = []
   for(var i = 1; i < musicians.length; i++){
     array.push(musicians[i] + " plays " + instruments[i])
-  
   return array;
 }
 }
